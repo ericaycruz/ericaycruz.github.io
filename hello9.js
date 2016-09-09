29bf0868c205482e8e7b9159aa2116c23804f9db
ericaycruz <w201273991@student.hccs.edu>
2016-09-08 20:50:45 -0500

    Add files via upload

---
+++ hello9.js
@@ -0,0 +1,7 @@
+
+	document.getElementById("header1").innerHTML = "Broken JavaScript Assignment! &#9786;";
+	
+	document.getElementById("header2").innerHTML = "You're getting close to done";
+	
+	document.getElementById("mycontent").innerHTML = "Wonderful!  I'm finally fixed!";
+	
